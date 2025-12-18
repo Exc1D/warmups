09d33a966d4b7e93709efda60a489432e348e4b0
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-18 21:16:43 +0800

    First commit

---
+++ shuffleArray.js
@@ -0,0 +1,6 @@
+// Write the function shuffle(array) that shuffles
+// (randomly reorders) elements of the array.
+
+// Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+let arr = [1, 2, 3];
