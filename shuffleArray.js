77180dafd927fff2fa2f4dec4d3c314c5e5edddf
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-18 21:35:53 +0800

    Feat: Use for reverse for loop,  math operations, and swap patterns

--- shuffleArray.js
+++ shuffleArray.js
@@ -4,3 +4,16 @@
 // Multiple runs of shuffle may lead to different orders of elements. For instance:
 
 let arr = [1, 2, 3];
+
+function shuffle(array) {
+  // Loop backwards and stop at 1
+  for (let i = array.length - 1; i > 0; i--) {
+    // Pick a random index from 0 to i
+    const j = Math.floor(Math.randow() * (i + 1));
+
+    const shuff = arr[i];
+    arr[i] = arr[j];
+    arr[j] = shuff;
+  }
+  return array;
+}
