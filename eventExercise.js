b0bdb3acfc777010a5a2114ae6cbee9f5f117eb8
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-19 21:37:12 +0800

    Feat: Add exercise functions

---
+++ eventExercise.js
@@ -0,0 +1,9 @@
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
