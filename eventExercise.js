27d0d383051399ea265ac98a9a1994482640d063
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-20 20:50:50 +0800

    Feat: Add exercise functions

--- eventExercise.js
+++ eventExercise.js
@@ -2,8 +2,13 @@ function alertFunction() {
   alert("YAY! YOU DID IT!");
 }
 
-const btn = document.querySelector("#btn");
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
 
-btn.addEventListener("click", function (e) {
-  e.target.style.background = "blue";
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
 });
--- script.js
+++
@@ -1,7 +0,0 @@
-function alertFunction() {
-  alert("YAY! YOU DID IT!");
-}
-
-const btn = querySelector("#btn");
-
-btn.addEventListener("click", alertFunction);
